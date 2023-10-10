0a71ee38c67a6e3a393cacd553904f298d1bd30d
Re13Liem <110783972+Re13Liem@users.noreply.github.com>
2023-10-10 16:19:24 +0700

    Cleare use image Dice

--- app/src/main/java/com/simbee/diceroller/MainActivity.kt
+++ app/src/main/java/com/simbee/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.simbee.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -30,7 +31,22 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.tvRoll)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imgDice)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {
+                R.drawable.dice_6
+            }
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
